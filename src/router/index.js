62189a713d32264c256e130a7d27242410ecb00f
hw <huweiwww@126.com>
2020-02-01 14:59:45 +0800

    项目初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+// 引入路由
+import movieRouter from './movie'
+import mineRouter from './mine'
+import cinemaRouter from './cinema'
 Vue.use(VueRouter)
-
+// 添加路由
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  movieRouter,
+  mineRouter,
+  cinemaRouter
+
 ]
 
 const router = new VueRouter({
